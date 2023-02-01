e6ee362da63286fcbeca95ef0b02dcc8cb7fbdd4
DeEpRo <DeEpRo@ASL-OPERATION>
2023-02-01 11:04:15 +0600

    Static files

--- deepro.BookStore/Controllers/HomeController.cs
+++ deepro.BookStore/Controllers/HomeController.cs
@@ -6,10 +6,16 @@ namespace deepro.BookStore.Controllers
     {
         public ViewResult Index()
         {
+            //var obj = new { Id = 1, Name = "deepro" };
             return View();
         }
 
         public ViewResult AboutUs()
+        {
+            return View();
+        }
+
+        public ViewResult ContactUs()
         {
             return View();
         }
